3c3200f73becd89a699e130f386a19f3c6993b0d
arpit294 <arpitvadhiyari84@gmail.com>
2026-09-09 16:27:08 +0530

    new

---
+++ header.php
@@ -0,0 +1,16 @@
+<!-- Main Content -->
+        <main class="main-content">
+            <header class="top-bar">
+                <h1 class="page-title"><?php echo $pagename; ?></h1>
+                <div class="header-actions">
+                    <button class="btn-icon">
+                        <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
+                            <path d="M18 8A6 6 0 006 8c0 7-3 9-3 9h18s-3-2-3-9" />
+                            <path d="M13.73 21a2 2 0 01-3.46 0" />
+                        </svg>
+                    </button>
+                    <div class="header-avatar">
+                        <img src="https://cdn-icons-png.flaticon.com/512/9187/9187604.png" alt="User">
+                    </div>
+                </div>
+            </header>
